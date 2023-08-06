cb9bfb23b78738ec9bb4b30a072e172c1cc6a08f
kevin-dorado <kevindorado2811@gmail.com>
2023-08-06 13:51:15 -0400

    admincarmake

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
+
+
+
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel)
 
 
 # Register your models here.
